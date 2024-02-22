5146170c9423580373d87da9a7375ad8e51ad6c5
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-22 18:01:41 +0300

    Feat:Added the final part of the main function
    I will have to create the next function

--- sieve_of_eratosthes.rs
+++ sieve_of_eratosthes.rs
@@ -10,5 +10,15 @@ fn main (){
     let limit: u32 = limit.trim().parse().unwrap();
     //List of all numbers from 2 to the limit
     let mut numbers: Vec<u32> = (2..limit).collect();
-    
+    //List of all prime numbers
+    let mut primes: Vec<u32> = Vec::new();
+
+    //Iterate through the numbers list
+    for number in numbers.iter(){
+        //If the number is prime, add it to the primes list
+        if is_prime(number){
+            primes.push(*number);
+        }
+    } 
 }
+//Function to check if a number is prime
