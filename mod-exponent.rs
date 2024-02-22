fb33efab9fefe0632a97a3e550a7c2043553af1d
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-22 14:28:51 +0300

    Feat:Added main function logic
    This was a pain in my ass

--- mod-exponent.rs
+++ mod-exponent.rs
@@ -4,20 +4,20 @@
 
 fn main() {
     //Allows user input for all three parameters
-    let mut _base = String::new();
+    let mut base = String::new();
     let mut exponent = String::new();
-    let mut _modu = String::new();
-    // println!("Enter the base: ");
-    // std::io::stdin().read_line(&mut base).unwrap();
+    let mut modu = String::new();
+    println!("Enter the base: ");
+    std::io::stdin().read_line(&mut base).unwrap();
     println!("Enter the exponent: ");
     std::io::stdin().read_line(&mut exponent).unwrap();
-    // println!("Enter the mod: ");
-    // std::io::stdin().read_line(&mut modu).unwrap();
+    println!("Enter the mod: ");
+    std::io::stdin().read_line(&mut modu).unwrap();
 
     //Converts the string to integers
-    // let base: i32 = base.trim().parse().unwrap();
+    let base: i32 = base.trim().parse().unwrap();
     let exponent: i32 = exponent.trim().parse().unwrap();
-    // let modu: i32 = modu.trim().parse().unwrap();
+    let modu: i32 = modu.trim().parse().unwrap();
     
     //Converts the exponent to binary
     let mut binary: Vec<i32> = Vec::new();
@@ -29,4 +29,8 @@ fn main() {
     binary.reverse();
     //Prints the binary
     println!("The Binary of the exponent : {:?}", binary);
+    //Prints the result
+    println!("The result of the mod-exponent algorithm is: {}", mod_exponent(base, binary, modu));
+ 
 }
+
