f41094d56eff1982f0d2c6585722ec58f68c7af5
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-22 14:34:16 +0300

    Feat:Got python to work
    I have just converted my beautiful rust code to Python

--- mod-exponent.py
+++ mod-exponent.py
@@ -2,18 +2,45 @@
 #It is supposed to take in three parameters, base, exponent, and mod, and return base^exponent mod
 #The algorithm is supposed to be implemented using the divide and conquer strategy
 #The algorithm is supposed to be implemented using recursion
+#I have just converted my Rust code to Python
 
-def mod_exponent(base, exponent, mod):
-    #Allow user to input the base, exponent, and mod
-    base = int(input("Enter the base: "))
-    exponent = int(input("Enter the exponent: "))
-    mod = int(input("Enter the mod: "))
+def mod_exponent(base, binary, modu):
+  result = 1
+
+  for bit in binary:
+    result = result * result % modu
+
+    if bit == 1:  
+      result = (base * result) % modu
+
+  return result
+
+def main():
+  base = input("Enter the base: ")
+  exponent = input("Enter the exponent: ") 
+  modu = input("Enter the mod: ")
+
+  base = int(base)
+  exponent = int(exponent)
+  modu = int(modu)
+
+  binary = []
+  exp = exponent
+  while exp > 0:
+    binary.append(exp % 2)
+    exp = exp // 2
+
+  binary.reverse()
+
+  print("The Binary of the exponent:", binary)
+
+  print("The result of the mod-exponent algorithm is:", 
+        mod_exponent(base, binary, modu))
+
+main()
+
     
-    #Converts the exponent to binary
-    binary = bin(exponent)
-    binary = binary[2:]
-    binary = binary[::-1]
-    print(binary)
+    
     
 
     
