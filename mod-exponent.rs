1842c3d9827d237b4037af3f02060cc49b7b61a2
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-22 13:54:16 +0300

    Feat:Added Rust equivalent
    This is because I just love Rust

---
+++ mod-exponent.rs
@@ -0,0 +1,32 @@
+//Implimenatation of the mod-exponent algorithm
+// This algorithm is suppoded take in three parameters, base, exponent, and mod, and return base^exponent mod
+// The algorithm is implemented using the divide and conquer approach
+
+fn main() {
+    //Allows user input for all three parameters
+    let mut _base = String::new();
+    let mut exponent = String::new();
+    let mut _modu = String::new();
+    // println!("Enter the base: ");
+    // std::io::stdin().read_line(&mut base).unwrap();
+    println!("Enter the exponent: ");
+    std::io::stdin().read_line(&mut exponent).unwrap();
+    // println!("Enter the mod: ");
+    // std::io::stdin().read_line(&mut modu).unwrap();
+
+    //Converts the string to integers
+    // let base: i32 = base.trim().parse().unwrap();
+    let exponent: i32 = exponent.trim().parse().unwrap();
+    // let modu: i32 = modu.trim().parse().unwrap();
+    
+    //Converts the exponent to binary
+    let mut binary: Vec<i32> = Vec::new();
+    let mut exp = exponent;
+    while exp > 0 {
+        binary.push(exp % 2);
+        exp = exp / 2;
+    }
+    binary.reverse();
+    //Prints the binary
+    println!("The Binary of the exponent : {:?}", binary);
+}
