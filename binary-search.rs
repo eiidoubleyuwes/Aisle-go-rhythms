ebd14e41b49c0bf6e02110ea6838dbb4c96eda21
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-26 00:30:50 +0300

    Feay commit changes wil not show

---
+++ binary-search.rs
@@ -0,0 +1,10 @@
+//This is my simple take on the binary search algorithm in Rust
+//The function takes in a vector of integers and a target integer
+//It returns the index of the target integer in the vector if it exists
+//Otherwise, it returns -1
+
+fn binary_search(arr: Vec<i32>, target: i32) -> i32 {
+    let mut left = 0;
+    let mut right = arr.len() - 1;
+   
+}
