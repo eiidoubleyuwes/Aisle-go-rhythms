89fd34706d69803a29e1de4d8c29f71daf1cc150
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-14 14:11:11 +0300

    Feat:Added user inupt
    Added the ability for the user to have an input into the algorithm

--- Euclideanalo.rs
+++ Euclideanalo.rs
@@ -4,4 +4,15 @@ The Algorithm works as follows:
 It uses GCD of the two numbers using the formula: b=aq+r and a = cq1+r1.
 This process continues until the last non-zero reminder
 
- */
+*/
+use std::io;
+
+fn main(){
+    println!("Hello there enter the first number you want to calculate the GCD");
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).expect("Failed to read value");
+    println!("Enter the second number you want to calculate the GCD");
+    let mut b = String::new();
+    io::stdin().read_line(&mut b).expect("Failed to read value");
+    println!("You have entered {} and {}",a,b);
+}
