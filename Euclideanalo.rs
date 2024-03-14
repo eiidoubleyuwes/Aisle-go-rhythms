31ecee996bb63c965d2a67a8219140cd548dcd8f
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-14 13:59:52 +0300

    Feat:Learning the Euclidean Algorithm
    Just added a brief description of how the algorithm should work

---
+++ Euclideanalo.rs
@@ -0,0 +1,7 @@
+//This is my take on the Euclidean Algorithm in rust
+/*
+The Algorithm works as follows:
+It uses GCD of the two numbers using the formula: b=aq+r and a = cq1+r1.
+This process continues until the last non-zero reminder
+
+ */
